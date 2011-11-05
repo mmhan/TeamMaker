2ba1cdb7c2b35ad0b98fa59ca80c72ee2dd18c6d
Mike Han <mmhan2u@gmail.com>
2011-11-05 17:41:21 +0800

    Baked projects

---
+++ app/controllers/projects_controller.php
@@ -0,0 +1,66 @@
+<?php
+class ProjectsController extends AppController {
+
+	var $name = 'Projects';
+
+	function admin_index() {
+		$this->Project->recursive = 0;
+		$this->set('projects', $this->paginate());
+	}
+
+	function admin_view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid project', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('project', $this->Project->read(null, $id));
+	}
+
+	function admin_add() {
+		if (!empty($this->data)) {
+			$this->Project->create();
+			if ($this->Project->save($this->data)) {
+				$this->Session->setFlash(__('The project has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The project could not be saved. Please, try again.', true));
+			}
+		}
+		$admins = $this->Project->Admin->find('list');
+		$this->set(compact('admins'));
+	}
+
+	function admin_edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid project', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->Project->save($this->data)) {
+				$this->Session->setFlash(__('The project has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The project could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Project->read(null, $id);
+		}
+		$admins = $this->Project->Admin->find('list');
+		$this->set(compact('admins'));
+	}
+
+	function admin_delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for project', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->Project->delete($id)) {
+			$this->Session->setFlash(__('Project deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Project was not deleted', true));
+		$this->redirect(array('action' => 'index'));
+	}
+}
+?>
